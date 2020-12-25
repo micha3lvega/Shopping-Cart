7780db783a38d741d2e64e2412e27cf9261d4519
Michael Vega <micha3lvega@gmail.com>
2020-12-25 14:29:18 -0500

    Agregar validaciones al objecto de categoria

--- product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/UnitOfMeasurementDTO.java
+++ product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/UnitOfMeasurementDTO.java
@@ -3,6 +3,9 @@ package co.com.micha3lvega.product.services.dto;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
@@ -20,6 +23,9 @@ public class UnitOfMeasurementDTO implements Serializable {
 	private static final long serialVersionUID = -356476957104031334L;
 
 	private String id;
+
+	@NotNull(message = "El nombre es obligatorio")
+	@Size(min = 2, message = "El nombre de la unidad de medida es muy corto")
 	private String name;
 
 	private Date createDate;
