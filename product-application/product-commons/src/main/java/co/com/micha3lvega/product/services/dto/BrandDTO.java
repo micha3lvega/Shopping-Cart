7cce4c29a617c7a2c4a3614c3c328c48b6b55064
Michael Vega <micha3lvega@gmail.com>
2020-12-24 21:05:20 -0500

    Correcion de constraint

--- product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/BrandDTO.java
+++ product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/BrandDTO.java
@@ -3,6 +3,9 @@ package co.com.micha3lvega.product.services.dto;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
@@ -20,6 +23,9 @@ public class BrandDTO implements Serializable {
 	private static final long serialVersionUID = -2576484819029903329L;
 
 	private String id;
+
+	@NotNull(message = "El nombre es obligatorio")
+	@Size(min = 2, message = "El nombre de la marca es muy corto")
 	private String name;
 
 	private Date createDate;
