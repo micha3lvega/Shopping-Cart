d3e17bb40de3a2a61ee9b6806a5d1981dbf0a64a
Michael Vega <micha3lvega@gmail.com>
2020-12-24 22:54:27 -0500

    Cambiar los mensajes de error

--- product-application/product-services/src/main/java/co/com/micha3lvega/product/services/exception/brand/BrandExistException.java
+++ product-application/product-services/src/main/java/co/com/micha3lvega/product/services/exception/brand/BrandExistException.java
@@ -3,7 +3,7 @@ package co.com.micha3lvega.product.services.exception.brand;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.CONFLICT, reason = "The brand already exists!")
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Ya existe una marca con este nombre")
 public class BrandExistException extends RuntimeException {
 
 	private static final long serialVersionUID = -4065653233110381228L;
--- product-application/product-services/src/main/java/co/com/micha3lvega/product/services/exception/brand/BrandNoExistException.java
+++ product-application/product-services/src/main/java/co/com/micha3lvega/product/services/exception/brand/BrandNoExistException.java
@@ -3,7 +3,7 @@ package co.com.micha3lvega.product.services.exception.brand;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "brand no exist!")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "La marca que busca no existe")
 public class BrandNoExistException extends RuntimeException {
 
 	private static final long serialVersionUID = 4793137409845098089L;
