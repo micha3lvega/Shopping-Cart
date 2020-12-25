b713eba64a47518fbd7874b0c0338ca0e47bc97b
Michael Vega <micha3lvega@gmail.com>
2020-12-25 18:39:34 -0500

    Agregar descripciones de swagger al controllador de categoria

--- product-application/product-services/src/main/java/co/com/micha3lvega/product/services/rest/controller/CategoryRestController.java
+++ product-application/product-services/src/main/java/co/com/micha3lvega/product/services/rest/controller/CategoryRestController.java
@@ -16,8 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import co.com.micha3lvega.product.services.dto.CategoryDTO;
 import co.com.micha3lvega.product.services.services.ICategoryServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
+@Api(tags = "Categorias", description = "Operaciones para la administracion de las categorias")
 @RequestMapping("/api/v1/category")
 public class CategoryRestController {
 
@@ -25,26 +28,31 @@ public class CategoryRestController {
 	private ICategoryServices services;
 
 	@GetMapping
+	@ApiOperation(produces = "application/json", notes = "Obtiene todos las categorias que hay en el sistema", value = "Obtener todas las categorias")
 	public List<CategoryDTO> findAll() {
 		return services.findAll();
 	}
 
 	@GetMapping("/{id}")
+	@ApiOperation(produces = "application/json", notes = "El ID de la categoria es obligatorio", value = "Obtener una categoria por su ID")
 	public CategoryDTO findById(@PathVariable("id") String id) {
 		return services.findById(id);
 	}
 
 	@PostMapping
+	@ApiOperation(value = "Crea una categoria", produces = "application/json")
 	public CategoryDTO insert(@Valid @RequestBody CategoryDTO category) {
 		return services.insert(category);
 	}
 
 	@PutMapping
+	@ApiOperation(value = "Actualiza una categoria", produces = "application/json", notes = "El id de la categoria es obligatorio")
 	public CategoryDTO update(@Valid @RequestBody CategoryDTO category) {
 		return services.update(category);
 	}
 
 	@DeleteMapping("/{id}")
+	@ApiOperation(value = "Elimina una categoria por su ID", produces = "application/json")
 	public void delete(@PathVariable("id") String id) {
 		services.delete(id);
 	}
