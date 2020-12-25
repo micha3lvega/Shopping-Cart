90ca597b8501d8dd86eceae94043829e7b9e3361
Michael Vega <micha3lvega@gmail.com>
2020-12-24 21:08:05 -0500

    Creacion del controlador para el objecto brand

---
+++ product-application/product-services/src/main/java/co/com/micha3lvega/product/services/rest/controller/BrandRestController.java
@@ -0,0 +1,39 @@
+package co.com.micha3lvega.product.services.rest.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.com.micha3lvega.product.services.dto.BrandDTO;
+import co.com.micha3lvega.product.services.services.IBrandServices;
+
+@RestController
+@RequestMapping("/api/v1/brand")
+public class BrandRestController {
+
+	@Autowired
+	private IBrandServices brandServices;
+
+	@GetMapping("/{id}")
+	public BrandDTO findById(@PathVariable("id") String id) {
+		return brandServices.findById(id);
+	}
+	
+	@PostMapping
+	public BrandDTO insert(@Valid @RequestBody BrandDTO brand) {
+		return brandServices.insert(brand);
+	}
+	
+	@PutMapping
+	public BrandDTO update(@Valid @RequestBody BrandDTO brand) {
+		return brandServices.update(brand);
+	}
+
+}
