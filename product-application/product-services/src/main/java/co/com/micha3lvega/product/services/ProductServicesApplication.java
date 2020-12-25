7e6bf784a76c89aec1683828cde8a573e7627897
Michael Vega <micha3lvega@gmail.com>
2020-12-24 19:13:52 -0500

    Creacion del proyecto product-application

---
+++ product-application/product-services/src/main/java/co/com/micha3lvega/product/services/ProductServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.product.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServicesApplication.class, args);
+	}
+
+}
