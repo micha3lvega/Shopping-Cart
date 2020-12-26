4a7fc137a8bff524428a71cd817956ab62498e5f
Michael Vega <micha3lvega@gmail.com>
2020-12-25 21:29:47 -0500

    Agregar propieades de swagger a la entidad imagen

--- product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/ImageDTO.java
+++ product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/ImageDTO.java
@@ -6,6 +6,9 @@ import java.io.Serializable;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiModelProperty.AccessMode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,16 +16,26 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonPropertyOrder({ "id", "name", "type", "data" })
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "id", "name", "type", "data" })
+@ApiModel(description = "Imagen del producto", value = "Imagen del producto")
 public class ImageDTO implements Serializable {
 
 	private static final long serialVersionUID = -3388733019000246496L;
 
+	@ApiModelProperty(required = true, value = "ID de la imagen", example = "5fe56033ebee2c5f3247c47e", accessMode = AccessMode.READ_ONLY)
 	private String id;
+	
+	@ApiModelProperty(value = "Tamaño de la imagen", example = "1246841", accessMode = AccessMode.READ_ONLY)
 	private Long size;
+	
+	@ApiModelProperty(required = true, value = "Nombre de la imagen", example = "Producto de ejemplo")
 	private String name;
+	
+	@ApiModelProperty(required = true, value = "Data base64 de la imagen", example = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABVYAAAM.....")
 	private InputStream data;
+	
+	@ApiModelProperty(required = true, value = "Content Type de la imagen", example = "PNG,JPG")
 	private String contentType;
 
 }
