3583a2770744dcf53f00d34916edc979a0dfe3c5
Michael Vega <micha3lvega@gmail.com>
2020-12-26 21:50:17 -0500

    Creacion de la aplicacion user-services

---
+++ user-application/user-services/src/main/java/co/com/micha3lvega/user/services/UserServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.user.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServicesApplication.class, args);
+	}
+
+}
