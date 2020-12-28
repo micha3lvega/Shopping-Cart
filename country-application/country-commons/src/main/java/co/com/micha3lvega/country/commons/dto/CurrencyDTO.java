c61908beebb7e66531fc5e07f6dd25576c6025f4
Michael Vega <micha3lvega@gmail.com>
2020-12-28 00:01:55 -0500

    Agregar constraints

--- country-application/country-commons/src/main/java/co/com/micha3lvega/country/commons/dto/CurrencyDTO.java
+++ country-application/country-commons/src/main/java/co/com/micha3lvega/country/commons/dto/CurrencyDTO.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,13 +30,18 @@ import lombok.NoArgsConstructor;
 public class CurrencyDTO implements Serializable {
 
 	private static final long serialVersionUID = -8234089146615325934L;
-
+	
+	@ApiModelProperty(required = true, value = "Codigo de la moneda", example = "Peso")
 	@JsonProperty("code")
 	private String code;
 
+	@NotNull
+	@ApiModelProperty(required = true, value = "Nombre de la moneda", example = "Peso")
 	@JsonProperty("name")
 	private String name;
 
+	@NotNull
+	@ApiModelProperty(required = true, value = "Simbolo de la moneda", example = "$")
 	@JsonProperty("symbol")
 	private String symbol;
 
