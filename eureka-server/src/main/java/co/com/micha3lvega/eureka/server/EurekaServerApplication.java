0671c245a750ef492ed237bb06d16207607f056a
Michael Vega <micha3lvega@gmail.com>
2020-12-27 20:58:00 -0500

    Creacion del servidor eureka

---
+++ eureka-server/src/main/java/co/com/micha3lvega/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package co.com.micha3lvega.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
