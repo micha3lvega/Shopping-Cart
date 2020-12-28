351c25e93d252002c0d05e64e51d495d2b379727
Michael Vega <micha3lvega@gmail.com>
2020-12-27 22:00:51 -0500

    Agregar configuracion de eureka-client al proyecto de user-services

--- user-application/user-services/src/main/java/co/com/micha3lvega/user/services/UserServicesApplication.java
+++ user-application/user-services/src/main/java/co/com/micha3lvega/user/services/UserServicesApplication.java
@@ -2,10 +2,12 @@ package co.com.micha3lvega.user.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableMongoAuditing
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServicesApplication {
 
 	public static void main(String[] args) {
