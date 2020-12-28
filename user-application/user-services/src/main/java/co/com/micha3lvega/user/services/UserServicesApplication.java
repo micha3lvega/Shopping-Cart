2ef2b6c0e27a004870575d48308c9eb53155da81
Michael Vega <micha3lvega@gmail.com>
2020-12-27 21:45:38 -0500

    Activar auditoria de mongo

--- user-application/user-services/src/main/java/co/com/micha3lvega/user/services/UserServicesApplication.java
+++ user-application/user-services/src/main/java/co/com/micha3lvega/user/services/UserServicesApplication.java
@@ -2,7 +2,9 @@ package co.com.micha3lvega.user.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongoAuditing
 @SpringBootApplication
 public class UserServicesApplication {
 
