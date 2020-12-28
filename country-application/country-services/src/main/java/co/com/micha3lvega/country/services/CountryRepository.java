202be2affee029d060f75e55fe0181c3126b4ba8
Michael Vega <micha3lvega@gmail.com>
2020-12-27 23:33:24 -0500

    Creacion del repositorio para el pais

--- country-application/country-services/src/main/java/co/com/micha3lvega/country/services/CountryRepository.java
+++ country-application/country-services/src/main/java/co/com/micha3lvega/country/services/CountryRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import co.com.micha3lvega.country.services.model.Country;
 
-public interface CountryRepository extends MongoRepository<Country, ID>{
+public interface CountryRepository extends MongoRepository<Country, String>{
 
 }
