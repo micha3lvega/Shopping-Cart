3b49ada0b555ce9aa45243e699fa166a016779bf
Michael Vega <micha3lvega@gmail.com>
2020-12-27 20:41:38 -0500

    Creacion del proyecto customer-services

---
+++ customer-application/customer-services/src/main/java/co/com/micha3lvega/customer/services/CustomerServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.customer.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServicesApplication.class, args);
+	}
+
+}
---
+++ customer-application/customer-services/src/test/java/co/com/micha3lvega/customer/services/CustomerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.customer.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
