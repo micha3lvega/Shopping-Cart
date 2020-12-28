3d3f71c536ab0d6c7594263ba283b910572c3315
Michael Vega <micha3lvega@gmail.com>
2020-12-27 22:04:54 -0500

    Configuracion de eureka-client para el proyecto product-services

--- product-application/product-services/src/main/java/co/com/micha3lvega/product/services/ProductServicesApplication.java
+++ product-application/product-services/src/main/java/co/com/micha3lvega/product/services/ProductServicesApplication.java
@@ -2,10 +2,12 @@ package co.com.micha3lvega.product.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableMongoAuditing
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServicesApplication {
 
 	public static void main(String[] args) {
