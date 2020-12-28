04b900c274ce12b898fc1ddce0b55095519b17b9
Michael Vega <micha3lvega@gmail.com>
2020-12-27 20:28:22 -0500

    Creacion de las clases de escucha de las excepciones de los servicios

---
+++ user-application/user-services/src/main/java/co/com/micha3lvega/user/services/exception/ErrorInfo.java
@@ -0,0 +1,38 @@
+package co.com.micha3lvega.user.services.exception;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ErrorInfo implements Serializable {
+
+	private static final long serialVersionUID = 925981040861588167L;
+
+	@JsonProperty("message")
+	private String message;
+
+	@JsonProperty("status_code")
+	private int statusCode;
+
+	@JsonProperty("uri")
+	private String uriRequested;
+
+	public ErrorInfo(int statusCode, String message, String uriRequested) {
+		this.message = message;
+		this.statusCode = statusCode;
+		this.uriRequested = uriRequested;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public String getUriRequested() {
+		return uriRequested;
+	}
+
+}
