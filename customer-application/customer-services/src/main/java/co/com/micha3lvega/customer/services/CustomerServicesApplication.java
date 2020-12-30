26f3691cd2aaf8b746b311cd682fb88fde4dcd05
Michael Vega <micha3lvega@gmail.com>
2020-12-30 15:18:12 -0500

    Configuracion para la activacion de los clientes feign

--- customer-application/customer-services/src/main/java/co/com/micha3lvega/customer/services/CustomerServicesApplication.java
+++ customer-application/customer-services/src/main/java/co/com/micha3lvega/customer/services/CustomerServicesApplication.java
@@ -3,9 +3,11 @@ package co.com.micha3lvega.customer.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class CustomerServicesApplication {
 
 	public static void main(String[] args) {
