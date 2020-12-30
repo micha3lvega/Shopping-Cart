40de35e9379dea48bdb5c43a6e52af9b6e792833
Michael Vega <micha3lvega@gmail.com>
2020-12-30 13:55:37 -0500

    Activar configuracion para la conexion con eureka-server

--- customer-application/customer-services/src/main/java/co/com/micha3lvega/customer/services/CustomerServicesApplication.java
+++ customer-application/customer-services/src/main/java/co/com/micha3lvega/customer/services/CustomerServicesApplication.java
@@ -2,8 +2,10 @@ package co.com.micha3lvega.customer.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServicesApplication {
 
 	public static void main(String[] args) {
