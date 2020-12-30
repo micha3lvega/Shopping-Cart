e50e9b9b52cc0fe82e3deddec72b727f1c950f74
Michael Vega <micha3lvega@gmail.com>
2020-12-30 15:18:43 -0500

    Creacion de los handlers para el manejo de errores al momento de llamar los servicios

---
+++ customer-application/customer-services/src/main/java/co/com/micha3lvega/customer/services/exception/handler/ErrorHandler.java
@@ -0,0 +1,39 @@
+package co.com.micha3lvega.customer.services.exception.handler;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import co.com.micha3lvega.customer.services.exception.ErrorInfo;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorInfo> methodArgumentNotValidException(HttpServletRequest request,
+			MethodArgumentNotValidException e) {
+
+		// get spring errors
+		BindingResult result = e.getBindingResult();
+		List<FieldError> fieldErrors = result.getFieldErrors();
+
+		// convert errors to standard string
+		StringBuilder errorMessage = new StringBuilder();
+		fieldErrors.forEach(f -> errorMessage.append(f.getDefaultMessage()));
+
+		// return error info object with standard json
+		ErrorInfo errorInfo = new ErrorInfo(HttpStatus.BAD_REQUEST.value(), errorMessage.toString(),
+				request.getRequestURI());
+		return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
+
+	}
+
+}
---
+++ customer-application/customer-services/src/main/java/co/com/micha3lvega/customer/services/exception/handler/ErrorHandlerCustomException.java
@@ -0,0 +1,22 @@
+package co.com.micha3lvega.customer.services.exception.handler;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import feign.FeignException;
+
+
+@ControllerAdvice
+public class ErrorHandlerCustomException {
+
+	@ExceptionHandler(FeignException.class)
+	public String methodArgumentNotValidException(FeignException e,  HttpServletResponse response) {
+		
+		response.setStatus(e.status());
+		
+		return e.getMessage();
+	}
+
+}
