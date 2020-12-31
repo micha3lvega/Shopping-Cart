4c5783d1a4778362551881da03984d975ca9c61c
Michael Vega <micha3lvega@gmail.com>
2020-12-31 01:33:03 -0500

    Creacion del repositorio para el carrito de compras

---
+++ shopping-cart-application/shopping-cart-services/src/main/java/co/com/micha3lvega/shopping/cart/services/repository/ShoppingCartRepository.java
@@ -0,0 +1,9 @@
+package co.com.micha3lvega.shopping.cart.services.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import co.com.micha3lvega.shopping.cart.services.model.ShoppingCart;
+
+public interface ShoppingCartRepository extends MongoRepository<ShoppingCart, String> {
+
+}
