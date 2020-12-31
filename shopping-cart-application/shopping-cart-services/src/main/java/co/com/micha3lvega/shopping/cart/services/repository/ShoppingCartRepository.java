f9e2bcab3c848838eeebf5a1ef96783c14cd717d
Michael Vega <micha3lvega@gmail.com>
2020-12-31 02:03:14 -0500

    Crear metodo para buscar por el id de un cliente

--- shopping-cart-application/shopping-cart-services/src/main/java/co/com/micha3lvega/shopping/cart/services/repository/ShoppingCartRepository.java
+++ shopping-cart-application/shopping-cart-services/src/main/java/co/com/micha3lvega/shopping/cart/services/repository/ShoppingCartRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import co.com.micha3lvega.shopping.cart.services.model.ShoppingCart;
 
 public interface ShoppingCartRepository extends MongoRepository<ShoppingCart, String> {
+	
+	ShoppingCart findByCustomerId(String customerId);
 
 }
