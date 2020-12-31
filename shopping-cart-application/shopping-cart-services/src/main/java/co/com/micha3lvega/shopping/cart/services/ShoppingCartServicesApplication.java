d85cd6e7d442ea40ba4d9f2cf41bd9a5b7640537
Michael Vega <micha3lvega@gmail.com>
2020-12-31 01:07:45 -0500

    Creacion del proyecto shopping-cart-services

---
+++ shopping-cart-application/shopping-cart-services/src/main/java/co/com/micha3lvega/shopping/cart/services/ShoppingCartServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.shopping.cart.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartServicesApplication.class, args);
+	}
+
+}
---
+++ shopping-cart-application/shopping-cart-services/src/test/java/co/com/micha3lvega/shopping/cart/services/ShoppingCartServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.shopping.cart.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
