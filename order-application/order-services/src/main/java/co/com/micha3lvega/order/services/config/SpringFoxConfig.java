4d6cd788f7f696a5de7272cc522283d2a148cede
Michael Vega <micha3lvega@gmail.com>
2020-12-31 20:19:45 -0500

    Creacion de la clase de configuracion de swagger

---
+++ order-application/order-services/src/main/java/co/com/micha3lvega/order/services/config/SpringFoxConfig.java
@@ -0,0 +1,33 @@
+package co.com.micha3lvega.order.services.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("co.com.micha3lvega.order.services.rest"))
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("API rest servicios de las ordenes", "Api para el manejo de los servicios de ordenes.",
+				"API TOS", "Terms of service", new Contact("Michael Vega", "www.example.com", "micha3lvega@gmail."),
+				"License of API", "API license URL", Collections.emptyList());
+	}
+
+}
