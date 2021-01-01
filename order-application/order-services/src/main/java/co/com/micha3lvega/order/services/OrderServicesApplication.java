41b9226ce610ef93a41947bd3b0fde59584e4580
Michael Vega <micha3lvega@gmail.com>
2020-12-31 19:19:59 -0500

    Creacion del proyecto order-services

---
+++ order-application/order-services/src/main/java/co/com/micha3lvega/order/services/OrderServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.order.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServicesApplication.class, args);
+	}
+
+}
---
+++ order-application/order-services/src/test/java/co/com/micha3lvega/order/services/OrderServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.order.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
