ab96cca3d20600643c738381e3992e057b699b22
Michael Vega <micha3lvega@gmail.com>
2020-12-31 20:12:48 -0500

    Agregar auditoria de mongo

--- shopping-cart-application/shopping-cart-services/src/main/java/co/com/micha3lvega/shopping/cart/services/ShoppingCartServicesApplication.java
+++ shopping-cart-application/shopping-cart-services/src/main/java/co/com/micha3lvega/shopping/cart/services/ShoppingCartServicesApplication.java
@@ -3,7 +3,9 @@ package co.com.micha3lvega.shopping.cart.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongoAuditing
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ShoppingCartServicesApplication {
