3558c76fa6d9aa65a417be79fdbc1d7469d94f2c
Michael Vega <micha3lvega@gmail.com>
2020-12-31 20:11:41 -0500

    Agregar auditoria de mongo

--- order-application/order-services/src/main/java/co/com/micha3lvega/order/services/OrderServicesApplication.java
+++ order-application/order-services/src/main/java/co/com/micha3lvega/order/services/OrderServicesApplication.java
@@ -3,7 +3,9 @@ package co.com.micha3lvega.order.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongoAuditing
 @SpringBootApplication
 @EnableDiscoveryClient
 public class OrderServicesApplication {
