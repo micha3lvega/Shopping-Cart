be12618afa5a7ad1017ebd4d1b330a0ee6d97f0d
Michael Vega <micha3lvega@gmail.com>
2020-12-31 20:11:20 -0500

    Agregar anotacion para la conexion con eureka-server

--- order-application/order-services/src/main/java/co/com/micha3lvega/order/services/OrderServicesApplication.java
+++ order-application/order-services/src/main/java/co/com/micha3lvega/order/services/OrderServicesApplication.java
@@ -2,8 +2,10 @@ package co.com.micha3lvega.order.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServicesApplication {
 
 	public static void main(String[] args) {
