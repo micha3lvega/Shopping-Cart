62c592bd5f123dcf4a9115a579e45b71d181d259
Michael Vega <micha3lvega@gmail.com>
2020-12-31 20:10:28 -0500

    Activar auditoria de mongo

--- country-application/country-services/src/main/java/co/com/micha3lvega/country/services/CountryServicesApplication.java
+++ country-application/country-services/src/main/java/co/com/micha3lvega/country/services/CountryServicesApplication.java
@@ -3,7 +3,9 @@ package co.com.micha3lvega.country.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongoAuditing
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CountryServicesApplication {
