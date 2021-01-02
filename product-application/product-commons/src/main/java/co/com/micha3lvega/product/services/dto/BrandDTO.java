d25a8aaf6ff63dcfb10af8a69e0a0b747c64e9be
Michael Vega <micha3lvega@gmail.com>
2021-01-02 01:23:59 -0500

    Correcion sonar duplicated blocks
    major
    Source files should not have any duplicated blocks

--- product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/BrandDTO.java
+++ product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/BrandDTO.java
@@ -1,43 +1,14 @@
 package co.com.micha3lvega.product.services.dto;
 
-import java.io.Serializable;
-import java.util.Date;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiModelProperty.AccessMode;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode(callSuper = false)
 @ApiModel(description = "Marca del producto", value = "Marca")
-@JsonPropertyOrder({ "id", "name", "create_date", "last_update" })
-public class BrandDTO implements Serializable {
+public class BrandDTO extends BasicObjectDTO {
 
 	private static final long serialVersionUID = -2576484819029903329L;
-
-	@ApiModelProperty(required = true, value = "ID de la marca", example = "5fe56033ebee2c5f3247c47e", accessMode = AccessMode.READ_ONLY)
-	private String id;
-
-	@NotNull(message = "El nombre es obligatorio")
-	@Size(min = 2, message = "El nombre de la marca es muy corto")
-	@ApiModelProperty(required = true, value = "Nombre de la marca", example = "Marca de ejemplo")
-	private String name;
-
-	@ApiModelProperty(value = "Fecha de creacion de la marca", example = "2020-12-25T23:08:29.735Z", accessMode = AccessMode.READ_ONLY)
-	private Date createDate;
-
-	@ApiModelProperty(value = "Fecha de ultima actualizacion de la marca", example = "2020-12-25T23:08:29.735Z", accessMode = AccessMode.READ_ONLY)
-	private Date lastUpdate;
 
 }
