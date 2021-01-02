a6638c5434ef46a692ffef33e794b3f3024b0681
Michael Vega <micha3lvega@gmail.com>
2021-01-02 01:10:20 -0500

    Correcion sonar (java:S1874)
    minor
    "@Deprecated" code should not be used
    (java:S1874)

--- product-application/product-services/src/main/java/co/com/micha3lvega/product/services/rest/controller/BrandRestController.java
+++ product-application/product-services/src/main/java/co/com/micha3lvega/product/services/rest/controller/BrandRestController.java
@@ -20,7 +20,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@Api(tags = "Marcas", description = "Operaciones para la administracion de las marcas")
+@Api(tags = "Marcas")
 @RequestMapping("/api/v1/brand")
 public class BrandRestController {
 
