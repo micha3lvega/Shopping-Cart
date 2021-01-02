1ff1776aa38ec297c1a990731b3bd3896675008e
Michael Vega <micha3lvega@gmail.com>
2021-01-02 01:10:39 -0500

    Correcion sonar
    minor
    Source files should not have any duplicated blocks

---
+++ product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/BasicObjectDTO.java
@@ -0,0 +1,39 @@
+package co.com.micha3lvega.product.services.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiModelProperty.AccessMode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "id", "name", "create_date", "last_update" })
+public class BasicObjectDTO implements Serializable{
+
+	@ApiModelProperty(required = true, value = "ID de la categoria", example = "5fe56033ebee2c5f3247c47e", accessMode = AccessMode.READ_ONLY)
+	private String id;
+
+	@NotNull(message = "El nombre es obligatorio")
+	@Size(min = 2, message = "El nombre de la categoria es muy corto")
+	@ApiModelProperty(required = true, value = "Nombre de la categoria", example = "Categoria de ejemplo")
+	private String name;
+
+	@ApiModelProperty(value = "Fecha de creacion de la categoria", example = "2020-12-25T23:08:29.735Z", accessMode = AccessMode.READ_ONLY)
+	private Date createDate;
+
+	@ApiModelProperty(value = "Fecha de ultima actualizacion de la categoria", example = "2020-12-25T23:08:29.735Z", accessMode = AccessMode.READ_ONLY)
+	private Date lastUpdate;
+
+}
--- product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/UnitOfMeasurementDTO.java
+++ product-application/product-commons/src/main/java/co/com/micha3lvega/product/services/dto/UnitOfMeasurementDTO.java
@@ -1,42 +1,13 @@
 package co.com.micha3lvega.product.services.dto;
 
-import java.io.Serializable;
-import java.util.Date;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiModelProperty.AccessMode;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode(callSuper = false)
 @ApiModel(description = "Unida de medida del producto", value = "Unidad de medida")
-@JsonPropertyOrder({ "id", "name", "create_date", "last_update" })
-public class UnitOfMeasurementDTO implements Serializable {
+public class UnitOfMeasurementDTO extends BasicObjectDTO {
 
 	private static final long serialVersionUID = -356476957104031334L;
-
-	@ApiModelProperty(required = true, value = "ID de la unidad de medida", example = "5fe56033ebee2c5f3247c47e", accessMode = AccessMode.READ_ONLY)
-	private String id;
-
-	@NotNull(message = "El nombre es obligatorio")
-	@Size(min = 2, message = "El nombre de la unidad de medida es muy corto")
-	@ApiModelProperty(required = true, value = "Nombre de la unidad de medida", example = "Unidad de medida de ejemplo")
-	private String name;
-
-	@ApiModelProperty(value = "Fecha de creacion de la unidad de medida", example = "2020-12-25T23:08:29.735Z", accessMode = AccessMode.READ_ONLY)
-	private Date createDate;
-
-	@ApiModelProperty(value = "Fecha de ultima actualizacion de la unidad de medida", example = "2020-12-25T23:08:29.735Z", accessMode = AccessMode.READ_ONLY)
-	private Date lastUpdate;
 }
