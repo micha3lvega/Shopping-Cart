7f6cbe818f33b0f7d6d42dbd93a6f172cb666fee
Michael Vega <micha3lvega@gmail.com>
2021-01-02 19:18:48 -0500

    Correcion sonar (java:S1874)
    minor
    "@Deprecated" code should not be used
    (java:S1874)

--- user-application/user-services/src/main/java/co/com/micha3lvega/user/services/rest/controller/UserRestController.java
+++ user-application/user-services/src/main/java/co/com/micha3lvega/user/services/rest/controller/UserRestController.java
@@ -20,7 +20,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@Api(tags = "Usuarios", description = "Operaciones para la administracion de los usuarios")
+@Api(tags = "Usuarios")
 @RequestMapping("/api/v1/user")
 public class UserRestController {
 
