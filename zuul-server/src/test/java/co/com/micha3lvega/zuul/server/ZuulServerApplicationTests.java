2ef9da39c3df2745dd9334d8972ec0d8a1006bb2
Michael Vega <micha3lvega@gmail.com>
2021-01-03 20:09:24 -0500

    Reconfiguracion del proyecto

---
+++ zuul-server/src/test/java/co/com/micha3lvega/zuul/server/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.zuul.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
