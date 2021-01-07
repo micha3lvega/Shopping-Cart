7f6944234ec5080cc72b72448def1c2f5af372af
Michael Vega <micha3lvega@gmail.com>
2021-01-07 18:42:47 -0500

    Eliminar clases de configuracion de oauth

--- zuul-server/src/main/java/co/com/micha3lvega/zuul/server/ZuulServerApplication.java
+++ zuul-server/src/main/java/co/com/micha3lvega/zuul/server/ZuulServerApplication.java
@@ -2,7 +2,9 @@ package co.com.micha3lvega.zuul.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulServerApplication {
 
